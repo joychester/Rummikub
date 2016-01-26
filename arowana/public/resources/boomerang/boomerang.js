b30ceb479ec4435dd37bf1b28ec81a4f4409bc04
Fred Xue <yufanfov@gmail.com>
2016-01-26 09:04:18 +0000

    Update Workflow and add check browser compability

--- arowana/public/resources/boomerang/boomerang.js
+++ arowana/public/resources/boomerang/boomerang.js
@@ -239,6 +239,8 @@ impl = {
     
     beacon_switch: true,
     
+    page_regex: "",
+    
 	events: {
 		"page_ready": [],
 		"page_unload": [],
@@ -392,7 +394,6 @@ boomr = {
 	    encodeString: function(str){
 	    	// Create Base64 Object
 			var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
-			
 			// Encode the String
 			return Base64.encode(str);
 		
@@ -715,7 +716,7 @@ boomr = {
 
 	init: function(config) {
 		var i, k,
-		    properties = ["beacon_url", "beacon_type", "site_domain", "user_ip", "strip_query_string", "secondary_beacons", "timeout", "beacon_switch"];
+		    properties = ["beacon_url", "beacon_type", "site_domain", "user_ip", "strip_query_string", "secondary_beacons", "timeout", "beacon_switch","page_regex"];
 
 		BOOMR_check_doc_domain();
 
@@ -728,14 +729,14 @@ boomr = {
 				impl[properties[i]] = config[properties[i]];
 			}
 		}
-
-		if(config.log !== undefined) {
-			this.log = config.log;
-		}
-		if(!this.log) {
-			this.log = function(/* m,l,s */) {};
-		}
 		
+		if(window.performance && window.performance.now && window.performance.timing && window.performance.getEntriesByType){
+			console.log("window.performance is supported");
+		}else{
+			console.log("BOOMR is not initiated because window.performance is not supported!");
+			return;
+		}
+
         if(impl.beacon_switch === true){
 			// set timeout to beacon
 			var t = impl.timeout;
@@ -744,6 +745,18 @@ boomr = {
 	                BOOMR.page_ready();
 	            }, t);
         }
+        
+        if(impl.page_regex !== ""){
+        	//filter page . to be continued
+        }
+        
+		if(config.log !== undefined) {
+			this.log = config.log;
+		}
+		if(!this.log) {
+			this.log = function(/* m,l,s */) {};
+		}
+		
 
 		for(k in this.plugins) {
 			if(this.plugins.hasOwnProperty(k)) {
@@ -1026,7 +1039,6 @@ boomr = {
 		     	BOOMR.debug("Hey I'm clearing vars."+ name);
 			}
 		}
-	
 	},
 
 	hasVar: function(name) {
