202da18362b3eb4669caa9f85b921c36e536321b
Cheng Chi (@joychester) <joychester@gmail.com>
2016-02-22 18:19:46 +0800

    hostname change

--- arowana/public/resources/boomerang/boomer_init.js
+++ arowana/public/resources/boomerang/boomer_init.js
@@ -3,6 +3,6 @@ BOOMR.init({
     autorun: false,
     timeout: 15000,
     beacon_switch: "ON", //ignore case
-    page_filter: "(^(http(s)?(:\/\/))?(www.)?)(slcd000ldr003.(com|co.uk|de|fr|c9users.io|stubcorp.com))([\/]?)",
+    page_filter: "(^(http(s)?(:\/\/))?(www.)?)(yourhostname.(com|co.uk|de|fr|c9users.io))([\/]?)",
     RT: {} //optional plugin config
 });
