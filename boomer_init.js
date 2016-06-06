f4cba8864255262d4a9363c6c472254f7c6ae7bd
joychester <joychester@gmail.com>
2016-06-06 13:50:12 -0700

    adding sh_boomr_ext.js

---
+++ boomer_init.js
@@ -0,0 +1,8 @@
+BOOMR.init({
+    //===boomerang original parameters===
+    beacon_url: "/rest/beacon",//or "https:\/\/www.hostname.com\/rest\/beacon"
+    autorun: false,
+
+    //===optional plugin configs===
+    RT: {}
+});
