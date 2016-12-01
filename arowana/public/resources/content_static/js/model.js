5fc616304ed298510fd83622d13df46a812d9798
Cheng Chi (@joychester) <joychester@gmail.com>
2016-12-01 23:32:25 +0800

    Update model.js

--- arowana/public/resources/content_static/js/model.js
+++ arowana/public/resources/content_static/js/model.js
@@ -1,65 +1,48 @@
-var app = angular.module('rum',[]);
+var app = angular.module('rum', []);
 
-app.controller('RUMController',function($scope, $http, $q){
-   $scope.changeNumber = function(){
-        $http.get('/rest/angular').then(function(response) {
-             if($('#ajaxEle').length<1) {
-                var mdiv=document.getElementById("message_div");
-                mdiv.innerHTML=response.data;
-                //adding Boomerang variables before page_ready event triggered
-                if("BOOMR" in window && "performance" in window) {
-                  BOOMR.addVar('user_timing',window.performance.now().toFixed(1));
+app.controller('RUMController', function ($scope, $http, $q) {
+    $scope.changeNumber = function () {
+        $http.get('/rest/angular').then(function (response) {
+            if ($('#ajaxEle').length < 1) {
+                var mdiv = document.getElementById("message_div");
+                mdiv.innerHTML = response.data;
+                if ("Rummikub" in window) {
+                    //Rummikub.addStopWatch('user_timing');
 
-                  //Add custom metrics Var if Resource timing API supported
-                  SHBoomrModule.addUMVar('t_css', 'loginCSS done blocking');
-                  SHBoomrModule.addUMVar('t_js', 'loginJS done blocking');
-                  SHBoomrModule.addUMVar('t_heroimg_loaded', 'hero img loaded');
-                  SHBoomrModule.addUMVar('t_heroimg_onload', 'hero img onload');
-
-                  //Trigger Page_ready event to send beacon
-                  BOOMR.page_ready();
+                    //Add custom metrics Var if Resource timing API supported
+                    Rummikub.addCustomMetrics('t_css', 'loginCSS done blocking');
                 }
-             } else {
-                 $('#ajaxEle').text('Your Passcode Generated: '+response.data);
-             };
+                //fire app:render-ready event
+                var myEvent = jQuery.Event( 'app:user-ready' );
+                $("body").trigger(myEvent);
+            } else {
+                $('#ajaxEle').text('Your Passcode Generated: ' + response.data);
+            };
         });
-   }
-
+    }
 });
 
 
-app.directive('myClick',function(){
+app.directive('myClick', function () {
     return {
         restrict: 'A',
         replace: false,
-        link: function(scope,element,attr){
-            element.bind("click", function(){
-                var funcName = attr.myClick.slice(0,-2);
-                // window.performance.clearMarks();
-                // window.performance.clearMeasures();
-                // window.performance.mark(funcName+"Start");
+        link: function (scope, element, attr) {
+            element.bind("click", function () {
+                var funcName = attr.myClick.slice(0, -2);
                 // the invoked function should return a promise. Otherwise, we don't know when the ajax call return. Promise.resolve() help us check the object type.
-               scope[funcName]();
-                //   window.performance.mark(funcName+"End");
-                //   window.performance.measure(funcName,funcName+"Start",funcName+"End");
-                //   console.log(window.performance.getEntriesByType('measure'));
-                //   BOOMR.sendMyData(funcName,window.performance.getEntriesByName(funcName)[0].duration.toFixed(1));
+                scope[funcName]();
             });
         }
     }
 });
 
 
-function loadData(){
+function loadData() {
     var scope = angular.element($("#ctrl")).scope();
-    scope.$apply(function(){
-        // window.performance.clearMarks();
-        // window.performance.clearMeasures();
+    scope.$apply(function () {
         // the invoked function should return a promise. Otherwise, we don't know when the ajax call return. Promise.resolve() help us check the object type.
         scope.changeNumber();
-        //   window.performance.mark("ajax3");
-        //   window.performance.measure("ajax3","navigationStart","ajax3");
-        //   console.log(window.performance.getEntriesByType('measure')); splunk
     })
 
 }
